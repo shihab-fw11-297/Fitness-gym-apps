ca7bae63c4f3f5ec5e0448f247136c8a3b59d952
shihab-fw11-297 <88572629+shihab-fw11-297@users.noreply.github.com>
2022-06-19 16:12:34 +0530

    create react application

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import { Box } from '@mui/material'
+
+const Navbar = () =>{
+    return(
+        <Box>
+            Navbar
+        </Box>
+    )
+}
+
+export default Navbar;
---
+++ src/pages/ExerciseDetail.jsx
@@ -0,0 +1,11 @@
+import { Box } from '@mui/material'
+
+const ExerciseDetail = () =>{
+    return(
+        <Box>
+            ExerciseDetail
+        </Box>
+    )
+}
+
+export default ExerciseDetail;
